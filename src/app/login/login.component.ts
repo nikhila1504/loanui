58a1e70d09da34f842c429561fcf66ef74b8a314
Damaraju <844419@cognizant.com>
2020-09-02 17:40:18 +0530

    search

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import {  Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +9,6 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class LoginComponent implements OnInit {
 
   constructor(    
-    private route: ActivatedRoute,
     private router: Router
 ) { }
 
